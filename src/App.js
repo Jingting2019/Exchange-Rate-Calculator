abaf84f6416e148ba9053da98b00eeff2648e9f4
Songyao <hsyyy789@gmail.com>
2020-06-18 22:26:04 -0400

    Finished with CSS

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import './App.css';
 import Option from './components/Option.js';
-// import logo from './logo.svg';
-// import './App.css';
+import logo from './images/money.png';
+
 
 class App extends Component {
   constructor(props) {
@@ -10,26 +11,27 @@ class App extends Component {
       items: {},
       currencyOne: "USD",
       currencyTwo: "EUR",
-      amountOne: "",
-      amountTwo: "",
+      amountOne: 0
     };
+
     this.handleChangeTop = this.handleChangeTop.bind(this);
+    this.updateCurrencyRate = this.updateCurrencyRate.bind(this);
   }
 
- //use bind
+  //use bind
   handleChangeTop(event) {
-    this.setState({currencyOne: event.target.value});
-    this.componentDidMount();
+    this.setState({ currencyOne: event.target.value });
+    this.updateCurrencyRate()
   }
 
-  swap = ()=> {
-    this.setState({currencyOne: this.state.currencyTwo, currencyTwo: this.state.currencyOne});
+  swap = () => {
+    this.setState({ currencyOne: this.state.currencyTwo, currencyTwo: this.state.currencyOne });
     console.log("before:" + this.state.currencyOne);
-    this.componentDidMount();
+    this.updateCurrencyRate()
     console.log("after:" + this.state.currencyOne);
   }
 
-  componentDidMount() {
+  updateCurrencyRate() {
     fetch(`https://api.exchangerate-api.com/v4/latest/${this.state.currencyOne}`)
       .then(res => res.json())
       .then(
@@ -41,33 +43,48 @@ class App extends Component {
       )
   }
 
+  componentDidMount() {
+    this.updateCurrencyRate()
+  }
+
   render() {
-    return(
-      <div>
+    return (
+      <div className="app">
+        <img src={logo}></img>
         <h1>Exchange Rate Calculator</h1>
         <p>Choose the currency and the amounts to get the exchange rate</p>
-        <div>
-          <select defaultValue="USD" value={this.state.currencyOne} onChange={this.handleChangeTop}>
-            <Option />
-          </select>
-          <button onClick={this.swap}>Swap</button>
-          {/*use arrow function to bind */}
-          <select  defaultValue="EUR" value={this.state.currencyTwo}
-          onChange={(e) => {
-            this.setState({currencyTwo: e.target.value});
-            this.componentDidMount();
-          }
-        }>
-            <Option />
-          </select>
+        <div className="exchanger">
+          <div className="amount labeled">
+              <label for="amount">Amount</label>
+              <input type="number" id="amount" placeholder={0} onChange={(e) => this.setState({ amountOne: e.target.value })} />
+          </div>
+          <div className="from labeled">
+            <label for="from">From</label>
+            <select id="from" defaultValue="USD" value={this.state.currencyOne} onChange={this.handleChangeTop}>
+              <Option />
+            </select>
+          </div>
+          <div className="swapButton">
+            <button onClick={this.swap}>Swap</button>
+          </div>
+          <div className="to labeled">
+            <label for="to">To</label>
+            {/*use arrow function to bind */}
+            <select id="to" defaultValue="EUR" value={this.state.currencyTwo}
+              onChange={(e) => {
+                this.setState({ currencyTwo: e.target.value });
+                this.componentDidMount();
+              }}>
+              <Option />
+            </select>
+          </div>
         </div>
-        <div>
-          <form>
-            <input type="number" value={this.state.value} onChange={(e) => this.setState({amountOne: e.target.value})}/>
-          </form>
-          <p>1{this.state.currencyOne} = {this.state.items[this.state.currencyTwo]} {this.state.currencyTwo}</p>
-          <p>result{this.state.items[this.state.currencyTwo] * this.state.amountOne}</p>
+        <p className="rate">1{this.state.currencyOne} = {this.state.items[this.state.currencyTwo]} {this.state.currencyTwo}</p>
+        <div className="result">
+          <h2>Result</h2>
+          <p>{ (this.state.items[this.state.currencyTwo] * this.state.amountOne).toFixed(2)}</p>
         </div>
+
       </div>
     )
   }
