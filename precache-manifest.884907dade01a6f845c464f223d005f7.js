074ea1e5a06e42f5096df4614e89d15cae659f3f
Jingting <fengshahuang@gmail.com>
2020-06-19 13:14:26 -0700

    Updates

---
+++ precache-manifest.884907dade01a6f845c464f223d005f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c1285beafa2863dca6b6f5a4120d715",
+    "url": "/Jingting2019/Exchange-Rate-Calculator/index.html"
+  },
+  {
+    "revision": "f41ab81367fd730f8391",
+    "url": "/Jingting2019/Exchange-Rate-Calculator/static/css/main.be39955c.chunk.css"
+  },
+  {
+    "revision": "c4b00e86bcc237534ac0",
+    "url": "/Jingting2019/Exchange-Rate-Calculator/static/js/2.e7af3a75.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Jingting2019/Exchange-Rate-Calculator/static/js/2.e7af3a75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f41ab81367fd730f8391",
+    "url": "/Jingting2019/Exchange-Rate-Calculator/static/js/main.5d1e29cf.chunk.js"
+  },
+  {
+    "revision": "44a382287367952a7da0",
+    "url": "/Jingting2019/Exchange-Rate-Calculator/static/js/runtime-main.2acbcca1.js"
+  },
+  {
+    "revision": "8ebfab8f167e6723aff8a3646414f245",
+    "url": "/Jingting2019/Exchange-Rate-Calculator/static/media/money.8ebfab8f.png"
+  }
+]);
